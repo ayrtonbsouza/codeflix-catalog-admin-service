47535735d606b4ae0704f5d293578aaaf632b431
Ayrton Souza <ayrtonsouza@icloud.com>
2025-11-10 19:18:59 +0000

    [:white_check_mark:] creating in memory implementation of searchable repository

--- src/shared/infra/db/in-memory/in-memory.repository.ts
+++ src/shared/infra/db/in-memory/in-memory.repository.ts
@@ -1,14 +1,22 @@
 import type { Entity } from '@/shared/domain/abstract/entity';
 import type { ValueObject } from '@/shared/domain/abstract/value-object';
 import { NotFoundError } from '@/shared/domain/errors/not-found.error';
-import type { IRepository } from '@/shared/domain/repositories/repository.interface';
+import type {
+  IRepository,
+  ISearchableRepository,
+} from '@/shared/domain/repositories/repository.interface';
+import type {
+  SearchParams,
+  SortDirection,
+} from '@/shared/domain/repositories/search-params';
+import { SearchResult } from '@/shared/domain/repositories/search-result';
 
 export abstract class InMemoryRepository<
   E extends Entity,
   EntityId extends ValueObject,
 > implements IRepository<E, EntityId>
 {
-  private entities: E[] = [];
+  protected entities: E[] = [];
 
   async findById(id: EntityId): Promise<E | null> {
     const item = this.entities.find((item) => item.entity_id.equals(id));
@@ -51,5 +59,84 @@ export abstract class InMemoryRepository<
     }
     this.entities.splice(foundIndex, 1);
     return Promise.resolve();
+  }
+}
+
+export abstract class InMemorySearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string,
+  >
+  extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
+{
+  sortableFields: string[] = [];
+
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    const filteredItems = await this.applyFilter(this.entities, props.filter);
+    const sortedItems = await this.applySort(
+      filteredItems,
+      props.sort,
+      props.sort_dir,
+    );
+    const paginatedItems = await this.applyPagination(
+      sortedItems,
+      props.page,
+      props.per_page,
+    );
+
+    return new SearchResult({
+      items: paginatedItems,
+      total: filteredItems.length,
+      current_page: props.page,
+      per_page: props.per_page,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: Filter | null,
+  ): Promise<E[]>;
+
+  protected applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+    custom_getter?: (sort: string, item: E) => unknown,
+  ): Promise<E[]> {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return Promise.resolve(items);
+    }
+
+    return Promise.resolve(
+      [...items].sort((a, b) => {
+        const aValue = custom_getter
+          ? custom_getter(sort, a)
+          : a[sort as keyof E];
+        const bValue = custom_getter
+          ? custom_getter(sort, b)
+          : b[sort as keyof E];
+
+        if (aValue < bValue) {
+          return sort_dir === 'asc' ? -1 : 1;
+        }
+
+        if (aValue > bValue) {
+          return sort_dir === 'asc' ? 1 : -1;
+        }
+
+        return 0;
+      }),
+    );
+  }
+
+  protected applyPagination(
+    items: E[],
+    page: SearchParams['page'],
+    per_page: SearchParams['per_page'],
+  ): Promise<E[]> {
+    const start = (page - 1) * per_page;
+    const limit = start + per_page;
+    return Promise.resolve(items.slice(start, limit));
   }
 }
