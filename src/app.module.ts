1ddb14118e2d2e570e37a0d5c470b17a731d6461
Ayrton Souza <ayrtonsouza@icloud.com>
2025-11-17 02:40:15 +0000

    [:construction:] integrating core module with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '@core/category/infra/db/sequelize/model/category.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite' as any,
+      host: ':memory:',
+      logging: false,
+      models: [CategoryModel],
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
